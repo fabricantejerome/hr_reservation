b502ebecd185b324d596b7f9c17b20ae80c29b71
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-20 07:36:28 +0800

    Login page

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -43,21 +43,21 @@
 						Error : Login Required!
 					</div> -->
 				<?php //endif; ?>
-
-				<?php //echo $message;?>
+				<?php //var_dump($this->session->userdata()); ?>
+				<?php echo isset($message) ? $message : ''  ?>
 				<h4 class="text-center">HR Training Room Reservation</h4>
 				<p class="login-box-msg">Sign in to start your session</p>
-				<?php echo form_open('login/check_login');?>
+				<?php echo form_open('index.php/login/authenticate');?>
 					
 					<div class="form-group has-feedback">
 						<input type="text" class="form-control" placeholder="Username" name="username" id="username" value="<?php echo set_value('username');?>"/>
 						<span class="glyphicon glyphicon-user form-control-feedback"></span>
-						<span class="help-block login-error-msg text-red"><?php echo form_error('username');?></span>
+						<span class="help-block login-error-msg text-red"><?php echo form_error('username'); ?></span>
 					</div>
 					<div class="form-group has-feedback">
 						<input type="password" class="form-control" placeholder="Password" name="password" id="password" value="<?php echo set_value('password');?>"/>
 						<span class="glyphicon glyphicon-lock form-control-feedback"></span>
-						<span class="help-block login-error-msg text-red"><?php echo form_error('password');?></span>
+						<span class="help-block login-error-msg text-red"><?php echo form_error('password'); ?></span>
 					</div>
 					<div class="row">
 						<div class="col-xs-8"></div>
