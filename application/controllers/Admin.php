557bb63ae0d751b9b4c499abe532587ada39d437
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-20 07:33:17 +0800

    Create controller for Admin

---
+++ application/controllers/Admin.php
@@ -0,0 +1,68 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		// Load helpers
+		$helpers = array('form');
+
+		$this->load->helper($helpers);
+
+		// Load Model
+		$this->load->model('room_model', 'rooms');
+	}
+
+	public function dashboard()
+	{
+		$data = array(
+			'title'   => 'Administrator Dashboard',
+			'content' => 'dashboard_view'
+		);
+
+		$this->load->view('include/template', $data);
+	}
+
+	public function rooms() 
+	{
+		$data = array(
+			'title'   => 'List of Rooms',
+			'content' => 'rooms_view',
+			'rooms'   => $this->rooms->browse()
+		);
+
+		$this->load->view('include/template', $data);
+	}
+
+	public function room_form()
+	{
+		$id = $this->uri->segment(3) ? $this->uri->segment(3) : 0;
+
+		$data = array(
+				'title'   => $id ? 'Edit Room Details' : 'Add Room',
+				'content' => 'room_form_view',
+				'room'    => $id ? $this->rooms->read($id) : 0
+			);
+
+		$this->load->view('include/template', $data);
+	}
+
+	public function room_store()
+	{
+		$this->rooms->store();
+
+		redirect('index.php/admin/rooms');
+	}
+
+	public function room_delete()
+	{
+		$id = $this->uri->segment(3) ? $this->uri->segment(3) : 0;
+
+		$this->rooms->delete($id);
+
+		redirect('index.php/admin/rooms');
+	}
+}
