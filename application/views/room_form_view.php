c46185bff846e275354727b6f063e73db0a9629f
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-20 07:36:18 +0800

    Form for adding and updating room

---
+++ application/views/room_form_view.php
@@ -0,0 +1,47 @@
+<?php 
+	//var_dump($room);
+?>
+<style type="text/css">
+	/* Control Button */
+	.control-btn > .btn.btn-flat {
+		margin: 10px;
+	}
+</style>
+<section class="content rooms">
+	<div class="row">
+		<div class="col-md-3">
+			<div class="box box-danger">					
+				<div class="box-body">
+					<!-- Form -->
+					<form action="<?php echo base_url('index.php/admin/room_store'); ?>" method="post">
+						<div class="form-group">
+							<label for="room_no">Room No.</label>
+							<input type="number" class="form-control" id="room_no" name="room_no" value="<?php echo isset($room['room_no']) ? $room['room_no'] : ''; ?>" required>
+						</div>
+
+						<div class="form-group">
+							<label for="room_name">Room</label>
+							<input type="text" class="form-control" id="room_name" name="room_name" value="<?php echo isset($room['room_name']) ? $room['room_name'] : ''; ?>" required>
+						</div>
+
+						<div class="form-group">
+							<label for="capacity">Capacity</label>
+							<input type="number" class="form-control" id="capacity" name="capacity" value="<?php echo isset($room['capacity']) ? $room['capacity'] : ''; ?>" required>
+						</div>
+
+						<div class="form-group">
+							<label for="description">Description</label>
+							<textarea class="form-control" id="description" name="description" rows="3"><?php echo isset($room['description']) ? $room['description'] : ''; ?></textarea>
+
+							<input type="text" class="form-control hidden" name="id" value="<?php echo isset($room['id']) ? $room['id'] : 0; ?>">
+						</div>
+
+						<div class="form-group">
+							<input type="submit" value="Submit" class="btn btn-flat btn-danger">
+						</div>
+					</form><!-- End Form -->
+				</div>
+			</div>
+		</div>	
+	</div>
+</section>
