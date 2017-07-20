0f03e2fb8729afb3737a7bcce6c2a686b93f9637
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-20 12:46:21 +0800

    Add room disapproved form view

---
+++ application/views/room_disapproved_form_view.php
@@ -0,0 +1,25 @@
+<section class="content rooms">
+	<div class="row">
+		<div class="col-md-3">
+			<div class="box box-danger">					
+				<div class="box-body">
+					<!-- Form -->
+					<form action="<?php echo base_url('index.php/admin/disapproved_request'); ?>" method="post">
+
+						<div class="form-group">
+							<label for="reason">Reason</label>
+							<textarea class="form-control" id="reason" name="reason" rows="3" required></textarea>
+
+							<input type="text" class="form-control hidden" name="room_res_id" value="<?php echo $room_res_id ?>">
+						</div>
+
+						<div class="form-group">
+							<input type="submit" value="Submit" class="btn btn-flat btn-danger">
+						</div>
+					</form>
+					<!-- End Form -->
+				</div>
+			</div>
+		</div>	
+	</div>
+</section>
