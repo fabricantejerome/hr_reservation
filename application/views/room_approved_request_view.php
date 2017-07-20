5dcd83a4fe2035690b71d4e4ea30b1924b62b504
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-20 11:22:39 +0800

    Create view for approved request

---
+++ application/views/room_approved_request_view.php
@@ -0,0 +1,48 @@
+<?php //var_dump($rooms); ?>
+
+<section class="content rooms">
+	<div class="row">
+		<div class="col-md-10">
+			<div class="box box-danger">
+					
+				<div class="box-body">
+					<!-- Room table -->
+					<table id="room_pending_tbl" class="table table-condensed table-striped table-bordered" >
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Room No.</th>
+								<th>Purpose</th>
+								<th>Reservation Date</th>
+								<th>Time Start</th>
+								<th>Time End</th>
+								<th>Date Reserved</th>
+								<th>Reserved by</th>
+								<th>Date Approved</th>
+								<th>Approved by</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php $counter = 1; ?>
+							<?php foreach($rooms as $row): ?>
+								<tr>
+									<td><?php echo $counter ?></td>
+									<td><?php echo $row->room_no ?></td>
+									<td><?php echo $row->purpose ?></td>
+									<td><?php echo $row->date_reserved ?></td>
+									<td><?php echo $row->time_start ?></td>
+									<td><?php echo $row->time_end ?></td>
+									<td><?php echo $row->date_reserved ?></td>
+									<td><?php echo $row->fullname ?></td>
+									<td><?php echo $row->approved_datetime ?></td>
+									<td><?php echo $row->approver ?></td>
+								</tr>
+								<?php $counter++; ?>
+							<?php endforeach; ?>
+						</tbody>
+					</table><!-- End of table -->
+				</div>
+			</div>
+		</div>	
+	</div>
+</section>
