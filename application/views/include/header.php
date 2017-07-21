ada11cfd3a3cf2a5edd1b515386271a17ec58244
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-21 11:12:03 +0800

    Display user fullname

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -25,14 +25,14 @@
 						<!-- The user image in the navbar-->
 						<img src="<?php echo base_url('resources/images/default.png');?>" class="user-image" alt="User Image">
 						<!-- hidden-xs hides the username on small devices so only the image appears. -->
-						<span class="hidden-xs"><?php //echo $this->session->get_userdata()['fullname'];?></span>
+						<span class="hidden-xs"><?php echo $this->session->userdata('fullname');?></span>
 					</a>
 					<ul class="dropdown-menu">
 						<!-- The user image in the menu -->
 						<li class="user-header">
 							<img src="<?php echo base_url('resources/images/default.png');?>" class="img-circle" alt="User Image">
 							<p>
-								<?php //echo $this->session->get_userdata()['fullname'];?>
+								<?php echo $this->session->userdata('fullname');?>
 							</p>
 						</li>
 				
