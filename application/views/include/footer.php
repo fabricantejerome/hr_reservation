bab5597b26ce7dfd2316fc63a877a28423644c24
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-21 11:11:42 +0800

    Change the year to 2017

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -8,5 +8,5 @@
 <!--
 	<strong>System By Management Information System Section</strong>
 -->
-	<strong>&copy; 2016 <a href="http://www.isuzuphil.com/"> &nbsp; Isuzu Philippines Corporation</a></strong> &nbsp;  All rights reserved.
+	<strong>&copy; 2017 <a href="http://www.isuzuphil.com/"> &nbsp; Isuzu Philippines Corporation</a></strong> &nbsp;  All rights reserved.
 </footer>
