d2aa508d5a7c387c2311ebd1a6667b1669bdff04
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-21 13:07:44 +0800

    View for cancelled data

---
+++ application/views/room_cancelled_request_view.php
@@ -0,0 +1,52 @@
+<?php //var_dump($rooms); ?>
+
+<section class="content rooms">
+	<div class="row">
+		<div class="col-md-10">
+			<div class="box box-danger">
+				<div class="box-body">
+					<!-- Room table -->
+					<table id="cancelled-request" class="table table-condensed table-striped table-bordered" >
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Room No.</th>
+								<th>Purpose</th>
+								<th>Date Reserved</th>
+								<th>Time Start</th>
+								<th>Time End</th>
+								<th>Reserved by</th>
+								<th>Date Cancelled</th>
+								<th>Cancelled by</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php $counter = 1; ?>
+							<?php foreach($requests as $row): ?>
+								<tr>
+									<td><?php echo $counter ?></td>
+									<td><?php echo $row->room_no ?></td>
+									<td><?php echo $row->purpose ?></td>
+									<td><?php echo date('m/d/Y', strtotime($row->date_reserved)); ?></td>
+									<td><?php echo $row->time_start ?></td>
+									<td><?php echo $row->time_end ?></td>
+									<td><?php echo $row->fullname ?></td>
+									<td><?php echo date('m/d/Y H:i:s', strtotime($row->cancelled_datetime)) ?></td>
+									<td><?php echo $row->approver ?></td>
+								</tr>
+								<?php $counter++; ?>
+							<?php endforeach; ?>
+						</tbody>
+					</table><!-- End of table -->
+				</div>
+			</div>
+		</div>	
+	</div>
+</section>
+<script type="text/javascript">
+	$(document).ready(function() {
+
+		$('#cancelled-request').DataTable();
+		
+	})
+</script>
