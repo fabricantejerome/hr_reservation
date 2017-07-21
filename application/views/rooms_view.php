d7e7ca6b8a42f7e389de1ff46e4b370f3d065a6a
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-21 11:13:09 +0800

    Display action menu when admin

--- application/views/rooms_view.php
+++ application/views/rooms_view.php
@@ -2,11 +2,14 @@
 	<div class="row">
 		<div class="col-md-6">
 			<div class="box box-danger">
-				<div class="box-header with-border">
-					<a href="<?php echo base_url('index.php/admin/room_form') ?>">
-						<button class="btn btn-flat btn-danger pull-right">Add Room <i class="fa fw fa-plus" aria-hidden="true"></i></button>
-					</a>
-				</div>
+				<?php if ($this->session->userdata('user_type') == 'admin'): ?>
+					<div class="box-header with-border">
+						<?php echo $this->session->flashdata('success_message'); ?>
+						<a href="<?php echo base_url('index.php/admin/room_form') ?>">
+							<button class="btn btn-flat btn-danger pull-right">Add Room <i class="fa fw fa-plus" aria-hidden="true"></i></button>
+						</a>
+					</div>
+				<?php endif; ?>
 					
 				<div class="box-body">
 					<!-- Room table -->
@@ -17,8 +20,10 @@
 								<th>Room No.</th>
 								<th>Room</th>
 								<th>Capacity</th>
-								<th></th>
-								<th></th>
+								<?php if ($this->session->userdata('user_type') == 'admin'): ?>
+									<th></th>
+									<th></th>
+								<?php endif; ?>
 							</tr>
 						</thead>
 						<tbody>
@@ -29,12 +34,14 @@
 									<td><?php echo $row->room_no; ?></td>
 									<td><?php echo $row->room_name; ?></td>
 									<td><?php echo $row->capacity; ?></td>
-									<td>
-										<a href="<?php echo base_url('index.php/admin/room_form/' . $row->id) ?>"><i class="fa fa-pencil" aria-hidden="true"></i></a>
-									</td>
-									<td>
-										<a href="<?php echo base_url('index.php/admin/room_delete/' . $row->id) ?>"><i class="fa fa-trash" aria-hidden="true"></i></a>
-									</td>
+									<?php if ($this->session->userdata('user_type') == 'admin'): ?>
+										<td>
+											<a href="<?php echo base_url('index.php/admin/room_form/' . $row->id) ?>"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+										</td>
+										<td>
+											<a href="<?php echo base_url('index.php/admin/room_delete/' . $row->id) ?>"><i class="fa fa-trash" aria-hidden="true"></i></a>
+										</td>
+									<?php endif; ?>
 								</tr>
 								<?php $counter++; ?>
 							<?php endforeach; ?>
