b45446f5dc692c547b7c9cedf13505b3fd71a9c7
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-22 12:39:44 +0800

    Store users to data source

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -43,4 +43,15 @@ class User_model extends CI_Model {
 		return false;
 	}
 
+	public function store(array $params = array())
+	{
+		if (count($params) > 0)
+		{
+			$this->db->insert('users_tbl', $params);
+			return $this->db->insert_id();
+		}
+
+		return 0;
+	}
+
 }
