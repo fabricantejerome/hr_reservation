d03a9d02f914902ddec64b32b0baacb71902bbad
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-25 17:34:36 +0800

    Remove IPC right text footer

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,12 +1,4 @@
 <!-- Main Footer -->
 <footer class="main-footer">
-	<!-- To the right -->
-	<div class="pull-right hidden-xs">
-		 <strong class="text-red">IPC Portal</strong>
-	</div>
-	<!-- Default to the left -->
-<!--
-	<strong>System By Management Information System Section</strong>
--->
 	<strong>&copy; 2017 <a href="http://www.isuzuphil.com/"> &nbsp; Isuzu Philippines Corporation</a></strong> &nbsp;  All rights reserved.
 </footer>
