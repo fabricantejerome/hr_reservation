5261d9007e999f22872e191ebf48d625114ce6fc
Jerome <jerome-fabricante@isuzuphil.com>
2017-07-31 11:39:20 +0800

    Change smtp credentials

--- application/libraries/EmailerPHP.php
+++ application/libraries/EmailerPHP.php
@@ -13,15 +13,18 @@ class EmailerPHP extends PHPMailer {
 		parent::__construct();
 
 		$this->isSMTP();     									// Set mailer to use SMTP
-		$this->CharSet       = "iso-8859-1";
-		$this->Host          = 'smtp.office365.com';			// Specify main and backup SMTP servers
-		$this->SMTPAuth      = true;							// Enable SMTP authentication
-		$this->Username      = 'notification@isuzuphil.com';	// SMTP username
-		$this->Password      = '!PCNotifier1';					// SMTP password
-		$this->SMTPSecure    = 'tls';							// Enable TLS encryption, `ssl` also accepted
-		$this->Port          = 587;								// TCP port to connect to
-		$this->From          = 'notification@isuzuphil.com';
-		$this->FromName      = 'HR Training Room Reservation';
+		$this->CharSet    = "iso-8859-1";
+		$this->Host       = 'smtp.office365.com';			// Specify main and backup SMTP servers
+		$this->SMTPAuth   = true;							// Enable SMTP authentication
+		$this->Username = 'notification@isuzuphil.com';	// SMTP username
+		$this->Password   = '(IJN9ijn ';					// SMTP password
+		/*$this->Username   = 'jerome-fabricante@isuzuphil.com';
+		$this->Password   = 'NJI(nji9';*/
+		$this->SMTPSecure = 'tls';							// Enable TLS encryption, `ssl` also accepted
+		$this->Port       = 587;								// TCP port to connect to
+		$this->From       = 'notification@isuzuphil.com';
+		//$this->From       = 'jerome-fabricante@isuzuphil.com';
+		$this->FromName   = 'HR Training Room Reservation';
 		$this->isHTML(true); 									// Set 
 	}
 }
