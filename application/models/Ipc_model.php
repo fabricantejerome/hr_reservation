6cd391c70eea22a1ad8386e6ff28434eb6d0da1b
Jerome <jerome-fabricante@isuzuphil.com>
2017-08-01 14:14:48 +0800

    Change the parameter
    Return array instead of object

--- application/models/Ipc_model.php
+++ application/models/Ipc_model.php
@@ -9,12 +9,12 @@ class Ipc_model extends CI_Model {
 		$this->db = $this->load->database('ipc_central', true);
 	}
 
-	public function fetch_personal_info($emp_no)
+	public function fetch_personal_info($params)
 	{
 		$fields = array(
 				'a.id',
 				'a.employee_no',
-				"CONCAT(b.last_name,' ', b.first_name,', ',b.middle_name) AS fullname",
+				"CONCAT(b.first_name,' ', b.last_name) AS fullname",
 				'c.email AS requestor_email',
 				'd.ash',
 				'd.sh',
@@ -22,47 +22,70 @@ class Ipc_model extends CI_Model {
 				'd.dh'
 			);
 
-		$config = array(
-				'employee_no' => $emp_no
+		$data = '';
+
+		if (isset($params['id']) && $params['id']  > 0)
+		{
+			$config = array(
+				'a.id' => $params['id']
 			);
 
-		$query = $this->db->select($fields)
-				->from('employee_masterfile_tab AS a')
-				->join('personal_information_tab AS b', 'a.id = b.employee_id', 'LEFT')
-				->join('email_tab AS c', 'a.id = c.employee_id', 'LEFT')
-				->join('signatories_matrix_tab AS d', 'a.section_id = d.section_id', 'LEFT')
-				->where($config)
-				->get();
+			$query = $this->db->select($fields)
+					->from('employee_masterfile_tab AS a')
+					->join('personal_information_tab AS b', 'a.id = b.employee_id', 'LEFT')
+					->join('email_tab AS c', 'a.id = c.employee_id', 'LEFT')
+					->join('signatories_matrix_tab AS d', 'a.section_id = d.section_id', 'LEFT')
+					->where($config)
+					->get();	
 
-		return $query->row();
+			$data = $query->row_array();
+		}
+		else if(isset($params['employee_no']) && $params['employee_no']  > 0)
+		{
+			$config = array(
+				'a.employee_no' => $params['employee_no']
+			);
+
+			$query = $this->db->select($fields)
+					->from('employee_masterfile_tab AS a')
+					->join('personal_information_tab AS b', 'a.id = b.employee_id', 'LEFT')
+					->join('email_tab AS c', 'a.id = c.employee_id', 'LEFT')
+					->join('signatories_matrix_tab AS d', 'a.section_id = d.section_id', 'LEFT')
+					->where($config)
+					->get();	
+
+			$data = $query->row_array();
+		}
+
+		return isset($data) ? $data : '';
 	}
 
 	public function fetch_department_head($emp_no)
 	{
-		$emp = $this->fetch_personal_info($emp_no);
+		$emp = $this->fetch_personal_info(array('employee_no' => $emp_no));
 
 		$dept_head_id = 0;
 
-		if ($emp->ash > 0 && $emp->id != $emp->ash) {
-			$dept_head_id = $emp->ash;		
+		if ($emp['ash'] > 0 && $emp['id'] != $emp['ash']) {
+			$dept_head_id = $emp['ash'];		
 		}
-		else if ($emp->sh > 0 && $emp->id != $emp->sh) {
-			$dept_head_id = $emp->sh;
+		else if ($emp['sh'] > 0 && $emp['id'] != $emp['sh']) {
+			$dept_head_id = $emp['sh'];
 		}
-		else if ($emp->adh > 0 && $emp->id != $emp->adh) {
-			$dept_head_id = $emp->adh;
+		else if ($emp['adh'] > 0 && $emp['id'] != $emp['adh']) {
+			$dept_head_id = $emp['adh'];
 		}
-		else if ($emp->dh > 0 && $emp->id != $emp->dh) {
-			$dept_head_id = $emp->dh;
+		else if ($emp['dh'] > 0 && $emp['id'] != $emp['dh']) {
+			$dept_head_id = $emp['dh'];
 		}
 		else {
-			$dept_head_id = $emp->id;
+			$dept_head_id = $emp['id'];
 		}
 
 		$fields = array(
 				'a.id',
 				'a.employee_no',
-				"CONCAT(b.last_name,' ', b.first_name,', ',b.middle_name) AS fullname",
+				"CONCAT(b.first_name,' ', b.last_name) AS fullname",
 				'c.email AS supervisor_email'
 			);
 
@@ -77,7 +100,7 @@ class Ipc_model extends CI_Model {
 				->where($config)
 				->get();
 
-		return $query->row();
+		return $query->row_array();
 	}
 
 }
