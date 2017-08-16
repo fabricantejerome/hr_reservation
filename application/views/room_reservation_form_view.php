f61e0a640898509c8efb7747ae4ed86b124e810a
Jerome <jerome-fabricante@isuzuphil.com>
2017-08-16 15:27:29 +0800

    Add id

--- application/views/room_reservation_form_view.php
+++ application/views/room_reservation_form_view.php
@@ -155,7 +155,7 @@
 			</div>
 
 			<div class="modal-footer">
-				<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-default pull-left" data-dismiss="modal" id="modal-close">Close</button>
 				<button type="submit" class="btn btn-success" disabled id="modal-submit">Submit</button>
 			</div>
 		</div>
