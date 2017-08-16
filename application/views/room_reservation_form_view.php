9d4a35283bd378279b79386c3a9a5cf11a861210
Jerome <jerome-fabricante@isuzuphil.com>
2017-08-16 14:18:27 +0800

    Modal header

--- application/views/room_reservation_form_view.php
+++ application/views/room_reservation_form_view.php
@@ -91,7 +91,10 @@
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
 	<div class="modal-dialog">
 		<div class="modal-content">
-		
+			<div class="modal-header">
+				<h4 class="modal-title">Terms and Conditions</h4>
+			</div>
+
 		</div>
 	</div>
 </div>
