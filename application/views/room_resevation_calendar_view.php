cba0aa9dbcbe5a5a2664665215b1e7a299f051c7
Jerome <jerome-fabricante@isuzuphil.com>
2017-08-23 13:35:56 +0800

    Fix style indention

--- application/views/room_resevation_calendar_view.php
+++ application/views/room_resevation_calendar_view.php
@@ -2,10 +2,10 @@
 <link href="<?php echo base_url('resources/templates/AdminLTE-2.3.5/plugins/fullcalendar/fullcalendar.print.css');?>" rel="stylesheet" media="print">
 <link href="<?php echo base_url('resources/plugins/select2/css/select2.min.css') ?>" rel="stylesheet" >
 <style>
-.fc-day-grid-event > .fc-content {
-    white-space: normal;
-    font-size: 12px;
-}
+	.fc-day-grid-event > .fc-content {
+	    white-space: normal;
+	    font-size: 12px;
+	}
 </style>
 <!-- Main content -->
 <section class="content calendar">
